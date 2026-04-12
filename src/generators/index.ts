b01df801119dc045cd5783d1033a680ed1dbddd9
joaobuzolin <joao.buzolin@hypr.mobi>
2026-04-12 15:42:37 -0300

    refactor: remove unused downloadBlob from barrel export

--- src/generators/index.ts
+++ src/generators/index.ts
@@ -2,4 +2,4 @@ export { genDV360 } from './dv360';
 export { genXandr } from './xandr';
 export { genStackAdapt } from './stackadapt';
 export { genAmazonDSP } from './amazon';
-export { downloadBlob, downloadCSV, downloadXLSX } from './download';
+export { downloadCSV, downloadXLSX } from './download';
